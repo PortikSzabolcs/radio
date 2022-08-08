aeef349585aa65e4f084cd8ff50c803b54fec9aa
PortikSzabolcs <portik.szabolcs@student.ms.sapientia.ro>
2022-08-09 00:15:32 +0300

    profmlogo + radio 1 non transparent

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const artworks = [
-    { src: "https://www.radio1.hu/wp-content/themes/radio1/assets/dist/logo-150x150.png",   sizes: '150x150',   type: 'image/png' },
+    { src: "https://radio1.hu/wp-content/themes/radio1/assets/dist/img/favicon/android-icon-192x192.png",   sizes: '192x192',   type: 'image/png' },
     { src: "https://www.radiogaga.ro/wp-content/uploads/2022/04/GaGa_Logo_vilagos_hatterre.png", sizes: '400x400', type: 'image/svg' },
     { src: "https://myonlineradio.hu/public/uploads/radio_img/mr2-petofi-radio/play_250_250.jpg",sizes: '250x250', type: 'image/jpg' },
     { src: "https://retroradio.hu/wp-content/themes/retroradio/assets/dist/img/logo.png", sizes: '150x150', type: 'image/png' },
@@ -8,7 +8,7 @@ const artworks = [
     { src: "https://coolfm.hu/radio/wp-content/uploads/2022/01/log%C3%B3-v%C3%A9gleges_shadow.png", sizes: '150x150', type: 'image/png' },
     { src: "https://mlotgijtlucd.i.optimole.com/w:500/h:500/q:75/rt:fill/g:ce/https://oxys.group/app/oxygen_music_logo%20(1)_juz25g1r.png", sizes: '150x150', type: 'image/png' },
     { src: "https://onlinestream.live/logos/6227.png", sizes: '150x150', type: 'image/png' },
-    { src: "img/stations/profm-logo.jpg", sizes: '400x400', type: 'image/jpg' },
+    { src: "https://www.profm.ro/static/theme-repo/apple-touch-icon.png?v=2.0", sizes: '180x180', type: 'image/jpg' },
     { src: "https://www.kissfm.ro/assets/logo/kissfm-inverted.svg", sizes: '320x320', type: 'image/svg-xml' },
     { src: "https://www.onefm.ro/img/onefm-logo.png", sizes: '150x150', type: 'image/png' },
     { src: "https://www.radioimpuls.ro/img/logo.png", sizes: '250x147', type: 'image/png' },
