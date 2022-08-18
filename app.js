88a39ef1e141080a619432efc21e328ef3417d7a
PortikSzabolcs <portik.szabolcs@student.ms.sapientia.ro>
2022-08-18 18:45:41 +0300

    new design

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const artworks = [
     { src: "img/stations/petofi.jpg",sizes: '250x250', type: 'image/jpg' },
     { src: "img/stations/retro.png", sizes: '150x150', type: 'image/png' },
     { src: "img/stations/star.jpg", sizes: '250x250', type: 'image/jpg' },
-    { src: "img/stations/szepviz.jpg", sizes: '250x250', type: 'image/jpg' },
+    { src: "img/stations/szepvizfm.jpg", sizes: '250x250', type: 'image/jpg' },
     { src: "img/stations/coolfm.png", sizes: '150x150', type: 'image/png' },
     { src: "img/stations/oxygen.webp", sizes: '150x150', type: 'image/png' },
     { src: "img/stations/best-dance.png", sizes: '150x150', type: 'image/png' },
@@ -16,23 +16,36 @@ const artworks = [
     { src: "img/stations/kiss-energy.jpg", sizes: '150x150', type: 'image/jpg' }
 ];
 
-const nevek = [
-    "Rádió 1",
-    "Rádió Gaga",
-    "Petőfi Rádió",
-    "Retró Rádió",
-    "Star Rádió",
-    "Szépvíz FM",
-    "Cool FM",
-    "Oxigen Music",
-    "Best-Dance Radio",
-    "ProFM",
-    "Kiss FM",
-    "One FM",
-    "Radio Impuls",
-    "Kiss Pop",
-    "Kiss Energy"
+const radios = [
+    { name: "Válassz rádiót", logo: "img/apple-touch.png", audio:""},
+    { name: "Rádió 1", logo: "img/stations/radio1.png", audio: "https://icast.connectmedia.hu/5202/live.mp3"},
+    { name: "Rádió Gaga", logo: "img/stations/gaga.svg", audio: "https://securestreams5.autopo.st:1992/player"},
+    { name: "Petőfi Rádió", logo: "img/stations/petofi.jpg", audio: "https://icast.connectmedia.hu/4738/mr2.mp3"},
+    { name: "Retró Rádió", logo: "img/stations/retro.png", audio: "https://icast.connectmedia.hu/5001/live.mp3"},
+    { name: "Star Rádió", logo: "img/stations/star.jpg", audio: "http://live.starradio.ro:9000/;"},
+    { name: "Szépvíz FM", logo: "img/stations/szepvizfm.jpg", audio: "https://stream2.rdstrm.com/szepviz-radio.mp3"},
+    { name: "Cool FM", logo: "img/stations/coolfm.png", audio: "https://mediagw.e-tiger.net/stream/coolfm"},
+    { name: "Oxygen Music", logo: "img/stations/oxygen.webp", audio: "https://oxygenmusic.hu:8000/oxygenmusic_128"},
+    { name: "Best-Dance Radio", logo: "img/stations/best-dance.png", audio: "https://ssl.omegahost.ro/8088/;"},
+    { name: "ProFM", logo: "img/stations/profm.jpg", audio: "https://edge126.rcs-rds.ro/profm/profm.mp3"},
+    { name: "Kiss FM", logo: "img/stations/kissfm.svg", audio: "https://live.kissfm.ro/kissfm.aacp"},
+    { name: "One FM", logo: "img/stations/onefm.png", audio: "https://live.onefm.ro/onefm.aacp"},
+    { name: "Radio Impuls", logo: "img/stations/impuls.png", audio: "https://live.radio-impuls.ro/stream"},
+    { name: "Kiss-Pop", logo: "img/stations/kiss-pop.png", audio: "https://live.kissfm.ro/kiss.xtra"},
+    { name: "Kiss-Energy", logo: "img/stations/kiss-energy.jpg", audio: "https://live.kissfm.ro/kiss.energy"}
 ];
+
+let favorites = [];
+if(localStorage.getItem("favorites")){
+    let string = localStorage.getItem("favorites");
+    const arr = string.split(',');
+    arr.pop();
+    for(let i=0; i<arr.length; i++){
+        favorites.push(Number(arr[i]));
+    }
+    updateFavList();
+    console.log(favorites);
+}
 
 const mediaAPI = ('mediaSession' in navigator);
 let lastOnline = null;
@@ -78,65 +91,83 @@ function mediaSessionInit(){
     navigator.mediaSession.metadata.title = "Saját Rádió";
     navigator.mediaSession.metadata.artwork = [ { src: "radio/img/apple-touch.png", sizes: '192x192', type: 'image/png'} ];
     navigator.mediaSession.setActionHandler('nexttrack', () => {
-        let id = 0;
-        let max = document.getElementsByTagName('audio').length;
-        while(id < max){
-            if(!document.getElementsByTagName('audio')[id].paused){
-                if(id < max-1){
-                    if(id === 3) id++;
-                    document.getElementsByTagName('audio')[id+1].play();
-                }
-                else{
-                    document.getElementsByTagName('audio')[0].play();
-                }
-                break;
-            }
-            ++id;
-        }
     });
     navigator.mediaSession.setActionHandler('previoustrack', () => {
-        let id = 0;
-        let max = document.getElementsByTagName('audio').length;
-        while(id < max){
-            if(!document.getElementsByTagName('audio')[id].paused){
-                if(id > 0){
-                    if(id === 5) id--;
-                    document.getElementsByTagName('audio')[id-1].play();
-                }
-                else{
-                    document.getElementsByTagName('audio')[max-1].play();
-                }
-                break;
-            }
-            ++id;
+    });
+}
+
+function radioSelect(selected){
+    document.getElementById("title").innerText = radios[selected].name;
+    document.getElementById("big-logo").src = radios[selected].logo;
+    document.getElementById("audio").src = radios[selected].audio;
+    document.getElementById("audio").play();
+
+    let image = document.createElement("img");
+    let button = document.createElement("button");
+    button.style.float = "right";
+    button.appendChild(image);
+    document.getElementById("title").appendChild(button);
+    button.onclick = function () {
+        if(favorites.includes(selected)){
+            removeFromFavorites(selected);
+            image.src = "img/star.png";
+        } else{
+            addToFavorites(selected);
+            image.src = "img/star-filled.png";
         }
-    });
-}
+    }
+    if(favorites.includes(selected)){
+        image.src = "img/star-filled.png";
+    } else {
+        image.src = "img/star.png";
+    }
 
-for (let i = 0; i < document.getElementsByTagName('audio').length; i++) {
-    let audio = document.getElementsByTagName('audio')[i];
-    audio.addEventListener("play", function () {
-        startPlaying(i);
-    });
-}
-
-function startPlaying(playing) {
     if(mediaAPI){
         if(navigator.mediaSession.metadata == null) mediaSessionInit();
-        navigator.mediaSession.metadata.title = nevek[playing];
-        navigator.mediaSession.metadata.artwork = [artworks[playing]];
-    }
-    for (let i = 0; i < document.getElementsByTagName('audio').length; i++) {
-        if (!document.getElementsByTagName('audio')[i].paused && i !== playing) {
-            let source = document.getElementsByTagName('audio')[i].src;
-            document.getElementsByTagName('audio')[i].src = "";
-            document.getElementsByTagName('audio')[i].load();
-            document.getElementsByTagName('audio')[i].src = source;
-            break;
-        }
+        navigator.mediaSession.metadata.title = radios[selected].name;
+        navigator.mediaSession.metadata.artwork = [artworks[selected-1]];
     }
 }
 
+function addToFavorites(nr){
+    favorites.push(nr);
+    let string = "";
+    for(let i=0; i<favorites.length; i++){
+        string += favorites[i] + ',';
+    }
+    localStorage.setItem("favorites", string);
+    updateFavList();
+}
+
+function removeFromFavorites(nr){
+    const fav2 = [];
+    for(let i=0; i<favorites.length; i++){
+        if(favorites[i] !== nr) fav2.push(favorites[i]);
+    }
+    favorites = fav2;
+    let string = "";
+    for(let i=0; i<favorites.length; i++){
+        string += favorites[i] + ',';
+    }
+    localStorage.setItem("favorites", string);
+    updateFavList();
+}
+
+function updateFavList(){
+    document.getElementById("favorites").innerHTML = "<h1> Kedvencek </h1>";
+    for(let i=0; i<favorites.length; i++) {
+        let button = document.createElement("button");
+        let image = document.createElement("img");
+        image.src = radios[favorites[i]].logo;
+        image.classList.add("favoriteLogo");
+        button.classList.add("favoriteButton");
+        button.appendChild(image);
+        button.onclick = function (){
+            radioSelect(favorites[i]);
+        }
+        document.getElementById("favorites").appendChild(button);
+    }
+}
 
 function idozitoMenuBe(){
     document.getElementById("popup_menu").style.display = "block";
@@ -199,8 +230,16 @@ function themeSwitch() {
 function themeSet() {
     if(theme === "white"){
         document.body.style.backgroundImage = "linear-gradient(#cfc, white)";
+        document.body.style.color = "black";
+        for(let i=0; i<document.getElementsByTagName('button').length; i++) {
+            document.getElementsByTagName('button')[i].style.color = "black";
+        }
     } else{
         document.body.style.backgroundImage = "linear-gradient(#020, black)";
+        document.body.style.color = "white";
+        for(let i=0; i<document.getElementsByTagName('button').length; i++) {
+            document.getElementsByTagName('button')[i].style.color = "white";
+        }
     }
     localStorage.setItem("theme", theme);
 }
